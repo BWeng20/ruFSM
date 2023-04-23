d4f2585fa1cd4235bf3aa403a812b9f1df4e67d1
Bernd <Bernd.Wengenroth@gmx.de>
2023-04-23 13:13:38 +0200

    "main" demo tester now exists if SM was terminated.

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ fn main() {
     match reader::read_from_xml_file(args[1].clone()) {
         Ok(mut sm) => {
             sm.tracer.enableTrace(Trace::ALL);
-            let (_thread_handle, mut sender) = fsm::start_fsm(sm);
+            let (thread_handle, mut sender) = fsm::start_fsm(sm);
 
             let mut line = String::new();
             let stdin = io::stdin();
@@ -57,58 +57,65 @@ fn main() {
 
             loop {
                 thread::sleep(time::Duration::from_millis(200));
-                print!("\nEnter Event >>");
-                match stdout().flush() {
-                    _ => {}
-                }
-                line.clear();
-                match stdin.read_line(&mut line) {
-                    Ok(_s) => {
-                        if line.ends_with('\n') {
-                            line.pop();
-                            if line.ends_with('\r') {
+
+                if thread_handle.is_finished() {
+                    print!("\nSM finished!");
+                    // TODO: dump data from the "finish"
+                    process::exit(0);
+                } else {
+                    print!("\nEnter Event >>");
+                    match stdout().flush() {
+                        _ => {}
+                    }
+                    line.clear();
+                    match stdin.read_line(&mut line) {
+                        Ok(_s) => {
+                            if line.ends_with('\n') {
                                 line.pop();
-                            }
-                        }
-                        let line_lc = line.to_lowercase();
-                        if line_lc.starts_with("tron") && line.len() > 5 {
-                            handle_trace(&mut sender, &line_lc[5..], true);
-                        } else if line_lc.starts_with("troff") && line_lc.len() > 6 {
-                            handle_trace(&mut sender, &line_lc[6..], false);
-                        } else if !line_lc.eq("help") && !line.is_empty() {
-                            let event = Box::new(Event {
-                                name: line.clone(),
-                                etype: EventType::platform,
-                                sendid: 0,
-                                origin: empty_str.clone(),
-                                origintype: empty_str.clone(),
-                                invokeid: 1,
-                                data: None,
-                            });
-                            match sender.send(event) {
-                                Ok(_r) => {
-                                    // ok
-                                }
-                                Err(e) => {
-                                    eprintln!("Error sending event: {}", e);
-                                    eprintln!("Aborting...");
-                                    process::exit(-2);
+                                if line.ends_with('\r') {
+                                    line.pop();
                                 }
                             }
-                        } else {
-                            println!(r#"Usage:
+                            let line_lc = line.to_lowercase();
+                            if line_lc.starts_with("tron") && line.len() > 5 {
+                                handle_trace(&mut sender, &line_lc[5..], true);
+                            } else if line_lc.starts_with("troff") && line_lc.len() > 6 {
+                                handle_trace(&mut sender, &line_lc[6..], false);
+                            } else if !line_lc.eq("help") && !line.is_empty() {
+                                let event = Box::new(Event {
+                                    name: line.clone(),
+                                    etype: EventType::platform,
+                                    sendid: 0,
+                                    origin: empty_str.clone(),
+                                    origintype: empty_str.clone(),
+                                    invokeid: 1,
+                                    data: None,
+                                });
+                                match sender.send(event) {
+                                    Ok(_r) => {
+                                        // ok
+                                    }
+                                    Err(e) => {
+                                        eprintln!("Error sending event: {}", e);
+                                        eprintln!("Aborting...");
+                                        process::exit(-2);
+                                    }
+                                }
+                            } else {
+                                println!(r#"Usage:
   Use 'Tron <flag>' or 'Troff <flag>' to control trace-levels.
   E.g. enter: tron all
   To send events, type the name of the event and press enter.
   Remind that Events are case sensitive.
   To print this information enter 'help' or an empty line.
   "#);
+                            }
                         }
-                    }
 
-                    Err(e) => {
-                        eprintln!("Error: {}. aborting...", e);
-                        process::exit(-1);
+                        Err(e) => {
+                            eprintln!("Error: {}. aborting...", e);
+                            process::exit(-1);
+                        }
                     }
                 }
             }
